95f41418115c3ed48c8c17268869ac9d3ffe3c19
Kirill <kirill.panarin@gmail.com>
2015-02-18 20:19:00 +0200

    changes to readme.md

--- run_analysis.R
+++ run_analysis.R
@@ -1,35 +1,37 @@
 # loads tidy train or test data set
-loadDataSet <- function(xTrainFile, yTrainFile) {
+loadDataSet <- function(xFile, yFile) {
     # load list of all features
     featureNames <- as.character(read.table("features.txt", stringsAsFactors = F)[, 2])
-    featureNames <- gsub("[\\(|)]", featureNames, replacement = "")
-    featureNames <- gsub("-", featureNames, replacement = "_")
     
     # we want to load only std and mean features
     featuresToLoad <- grepl("std", featureNames) | grepl("mean", featureNames)
     colClassesToLoad <- rep("NULL", times = length(featureNames))
     colClassesToLoad[featuresToLoad] <- "numeric"
     
-    # load train data set
-    train <- read.table(xTrainFile, colClasses = colClassesToLoad, 
-                        col.names = featureNames)
+    # load data set
+    colNames <- gsub("[\\(|)]", featureNames, replacement = "")
+    colNames <- gsub("-", featureNames, replacement = "_")
+    
+    data <- read.table(xFile, colClasses = colClassesToLoad, 
+                        col.names = colNames)
+    names(data) <- featureNames[featuresToLoad]
     
     # load activity labels
     activityLabels <- read.table("activity_labels.txt", col.names = c("ActivityLabelIndex", "ActivityLabel"))
     
-    # load train activities
-    trainActivities <- read.table(yTrainFile, stringsAsFactors = T
+    # load activities
+    trainActivities <- read.table(yFile, stringsAsFactors = T
                                   , colClasses = c("numeric"), col.names = c("ActivityLabelIndex"))
     trainActivities <- merge(trainActivities, activityLabels, by = "ActivityLabelIndex")[, 2]
-    
-    # append activities to train data set
-    train$Activity <- trainActivities
-    train
+        
+    # append activities to data set
+    data$Activity <- trainActivities
+    data
 }
 
 # load and merge tidy train and test data sets
 data <- rbind(loadDataSet("train\\X_train.txt", "train\\y_train.txt"), 
                loadDataSet("test\\X_test.txt", "test\\y_test.txt"))
-write.table(data, "tidy.txt", row.names = F)
 
-# todo: include readme.md
+# save tidy data set to tidy.txt
+write.table(data, "tidy.txt", row.names = F)
