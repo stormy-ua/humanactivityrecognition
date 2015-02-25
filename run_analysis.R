fdc26b019993174bc1ad0d5382e7ae4265f9a232
Kirill <kirill.panarin@gmail.com>
2015-02-25 16:12:51 +0200

    several changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,8 @@
+library(dplyr)
+library(magrittr)
+
 # loads tidy train or test data set
-loadDataSet <- function(xFile, yFile) {
+loadDataSet <- function(xFile, yFile, subjectsFiles) {
     # load list of all features from features.txt
     featureNames <- as.character(read.table("features.txt", stringsAsFactors = F)[, 2])
     
@@ -12,14 +15,18 @@ loadDataSet <- function(xFile, yFile) {
     data <- read.table(xFile, colClasses = colClassesToLoad)
     # set column names
     names(data) <- featureNames[featuresToLoad]
-    
+        
+    # load subjects
+    subjects <- read.table(subjectsFiles, colClasses = c("numeric"), col.names = c("SubjectId"))  
+    data <- cbind(subjects, data)
+        
     # load activity labels from activity_labels.txt
     activityLabels <- read.table("activity_labels.txt", col.names = c("ActivityLabelIndex", "ActivityLabel"))
     
     # load activities
     activities <- read.table(yFile, stringsAsFactors = T
-                                  , colClasses = c("numeric"), col.names = c("ActivityLabelIndex"))
-        
+                             , colClasses = c("numeric"), col.names = c("ActivityLabelIndex"))
+    
     # append activities to data set
     activities <- merge(activities, activityLabels, by = "ActivityLabelIndex")[, 2]
     data$Activity <- activities
@@ -29,8 +36,13 @@ loadDataSet <- function(xFile, yFile) {
 }
 
 # load and merge tidy train and tidy test data sets
-data <- rbind(loadDataSet("train\\X_train.txt", "train\\y_train.txt"), 
-               loadDataSet("test\\X_test.txt", "test\\y_test.txt"))
+data <- rbind(loadDataSet("train\\X_train.txt", "train\\y_train.txt", "train\\subject_train.txt"), 
+               loadDataSet("test\\X_test.txt", "test\\y_test.txt", "test\\subject_test.txt"))
+
+#summarize
+data <- data %>% group_by(SubjectId, Activity) %>% summarise_each(funs(mean))
+
+head(data)
 
 # save tidy data set to tidy.txt
 write.table(data, "tidy.txt", row.names = F, quote = F)
